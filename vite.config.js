9e2474b89d1ad81f12ffa43e9531af6905cb3648
MahesaAlghifari <212310006@student.ibik.ac.id>
2026-09-09 12:59:33 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Admindashboarduiux/",
+  base: "/edussphere.sekolahss.sch.id/",
 
   plugins: [
     react(),
